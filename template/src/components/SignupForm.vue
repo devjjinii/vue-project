aaaaa212312b7475b87694ca480ce48ef762db9d
devjjinii <devjjinii@naver.com>
2020-11-14 19:12:17 +0900

    add signup

--- template/src/components/SignupForm.vue
+++ template/src/components/SignupForm.vue
@@ -8,7 +8,8 @@
 			<label for="password">pw: </label>
 			<input id="password" type="text" v-model="password" />
 		</div>
-		<button type="submit">login</button>
+		<button type="submit">회원 가입</button>
+		<p>{{ logMessage }}</p>
 	</form>
 </template>
 
@@ -21,6 +22,7 @@ export default {
 		return {
 			username: '',
 			password: '',
+			logMessage: '',
 		};
 	},
 	methods: {
@@ -30,9 +32,15 @@ export default {
 				username: this.username,
 				password: this.password,
 			};
-			console.log(userData);
-			const response = await registerUser(userData);
-			console.log(response);
+			// console.log(userData);
+			const { data } = await registerUser(userData);
+			console.log(data.username);
+			this.logMessage = `${data.username}님 가입을 환영합니다!`;
+			this.initForm();
+		},
+		initForm() {
+			this.username = '';
+			this.password = '';
 		},
 	},
 };
