1a8bcc212d21a0a4bebda2b642e3078fecb6bd8e
devjjinii <devjjinii@naver.com>
2020-11-15 19:39:06 +0900

    add loginForm

--- template/src/views/LoginPage.vue
+++ template/src/views/LoginPage.vue
@@ -1,10 +1,16 @@
 <template>
 	<div>
-		login
+		<h1>로그인 페이지</h1>
+		<LoginForm></LoginForm>
 	</div>
 </template>
 
 <script>
-export default {};
+import LoginForm from '@/components/LoginForm';
+export default {
+	components: {
+		LoginForm,
+	},
+};
 </script>
 <style></style>
